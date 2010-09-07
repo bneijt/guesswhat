51ed6a068c34b93c504a349c72e0f87bef3f701e
A. Bram Neijt <bneijt@xebia.com>
2010-09-07 09:43:23 +0200

    First working request through jetty
    http://localhost:8080/guesswhat/guesswhat/123
    
    TODO:
    - Clean up this mess...

--- src/main/java/bneijt/guesswhat/GuessWhat.java
+++ src/main/java/bneijt/guesswhat/GuessWhat.java
@@ -3,11 +3,12 @@ package bneijt.guesswhat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class GuessWhat {
-
+	@ResponseBody
 	@RequestMapping(value = "/guesswhat/{guess}")
 	public String guess(@PathVariable long guess) {
 		return "Hello";
