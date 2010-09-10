4d252379c6d8872572699db2edd9535a8a1cbde4
A. Bram Neijt <bneijt@xebia.com>
2010-09-10 12:21:35 +0200

    Clean up code, trim logging.

--- src/main/java/bneijt/guesswhat/GuessWhat.java
+++ src/main/java/bneijt/guesswhat/GuessWhat.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-//import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class GuessWhat {
 
@@ -37,28 +35,15 @@ public class GuessWhat {
 	
 	@RequestMapping(value = "/guess/{name}/{guess}")
 	public @ResponseBody boolean guess(@PathVariable String name, @PathVariable long guess) {
-		// Bare metal approach to json
 		SecretNumber secret = secrets.secretOf(name);
 		return secret.guess(guess);
 	}
 
 	@RequestMapping(value = "/people/list")
 	public ModelAndView peopleList() {
-		// Bare metal approach to html. Hackers, please inject your content
-		// here!
 		ModelAndView mav = new ModelAndView("peopleList");
 		mav.addObject("people", secrets.people());
 		return mav;
-		/*
-		String c = "<html><body><ul>";
-		for (String person : secrets.people()) {
-			c += "<li><span classname=\"person\">"
-					+ person
-					+ "</span>: <input type=\"text\" class=\"guessfield\" /></li>";
-		}
-		c += "</ul></body></html>";
-		return c;
-		*/
 	}
 
 	@RequestMapping(value = "/set/")
@@ -71,15 +56,10 @@ public class GuessWhat {
 		}
 		SecretNumber secret = this.secrets.rememberSecret(secretForm.getName(),
 				new LongSecretNumber(secretForm.getNumber()));
-		// model.put("secret", secret);
-		// return "result";
-		// Gson gson = new Gson();
-		// return gson.toJson(secret);
 		mav.addObject(secret);
 		return mav;
 
-		// return "form";
-		// TODO Set response content type!
+		// TODO Set response content type?
 	}
 
 }
